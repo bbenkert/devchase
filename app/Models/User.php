c7ff4a52d1a933e17140dd80727626db99e77bfe
Benjamin Benkert <29586970+bbenkert@users.noreply.github.com>
2025-04-14 08:46:00 -0400

    Initial commit: Laravel blog setup with Sail and Filament

---
+++ app/Models/User.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+
+class User extends Authenticatable implements FilamentUser
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    /**
+     * Determine if the user can access Filament.
+     *
+     * @return bool
+     */
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, 'ben@devchase.com');
+    }
+
+    /**
+     * Determine if the user can access the specified Filament panel.
+     *
+     * @param \Filament\Panel $panel
+     * @return bool
+     */
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true; // or use email domain checks if needed
+    }
+}
