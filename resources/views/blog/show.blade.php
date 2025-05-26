18b727e4a2dc4c541424006e0a33499522f7b8c5
Benjamin Benkert <29586970+bbenkert@users.noreply.github.com>
2025-05-25 21:19:09 -0400

    Update post show blade

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -6,7 +6,7 @@
             <img src="{{ asset('storage/' . $post->featured_image) }}" class="w-full h-75 object-cover mb-6 rounded shadow" />
         @endif
 
-        <div class="prose prose-lg max-w-none prose-img:rounded-lg prose-pre:bg-gray-100 prose-code:bg-gray-100 prose-code:px-1 prose-code:py-0.5">
+        <div class="prose prose-lg max-w-none prose-img:rounded-lg prose-headings:text-gray-800 prose-a:text-blue-600 hover:prose-a:underline prose-blockquote:border-l-4 prose-blockquote:border-gray-300 prose-blockquote:pl-4 prose-blockquote:italic prose-blockquote:text-gray-700 prose-blockquote:bg-gray-50 prose-blockquote:rounded-r-md prose-code:bg-rose-100 prose-code:text-rose-700 prose-code:px-1.5 prose-code:py-0.5 prose-code:rounded-md prose-code:font-mono prose-code:text-sm prose-pre:bg-slate-800 prose-pre:text-slate-200 prose-pre:p-4 prose-pre:rounded-lg prose-pre:shadow-md prose-pre:overflow-x-auto prose-pre:prose-code:bg-transparent prose-pre:prose-code:text-inherit prose-pre:prose-code:p-0 prose-pre:prose-code:rounded-none prose-pre:prose-code:font-mono prose-pre:prose-code:text-sm">
             {!! $post->renderedContent() !!}
         </div>
 
